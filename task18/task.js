3c5b517f1ac45b1dba96af97664ce83d397599a2
CoderLiu <337451700@qq.com>
2016-03-31 12:51:50 +0800

    提交任务18,任务19

--- task18/task.js
+++ task18/task.js
@@ -4,16 +4,27 @@
 var queueBox = document.getElementById("queue-box"),
     form = document.getElementById("form"),
     input = document.getElementById("ipt"),
-    queueHtml,   //存放动态生成的div
+    tempInputValue,
     dataStore = [];  //用来存放队列数据
 
 /**
+ *判断输入框内容是否是数字，若是，返回true，否则返回false
+ */
+function isNumber(){
+    tempInputValue = input.value;
+    if(isNaN(tempInputValue)){
+        alert("请输入数字！");
+        return false;
+    }else {
+        return tempInputValue;   //tempInputValue为空转为false，否则转为true
+    }
+}
+/**
 *左侧入队函数
- * html中输入框的type为number，所以这里只判断获取的输入框的值不为空
 */
 function leftEnqueue(){
-    if(input.value != ""){
-        dataStore.unshift(input.value);
+    if(isNumber()){
+        dataStore.unshift(tempInputValue);
     }
 }
 
@@ -21,8 +32,8 @@ function leftEnqueue(){
  *右侧入队函数
  */
 function rightEnqueue(){
-    if(input.value != ""){
-        dataStore.push(input.value);
+    if(isNumber()){
+        dataStore.push(tempInputValue);
     }
 }
 
@@ -52,16 +63,19 @@ function rightDequeue(){
  *渲染队列函数
  */
 function renderQueue(){
-    queueHtml = "";   //清空页面
+    var queueHtml = "";   //清空页面
 
     //遍历dataStore中的所有数据，渲染页面
     for(var key in dataStore){
-        queueHtml += "<div>"+dataStore[key]+"</div>"
+        queueHtml += "<div class="+key+">"+dataStore[key]+"</div>"
     }
     queueBox.innerHTML = queueHtml;
 }
-function changeQueue(){
 
+/**
+ * 监听队列中数字的变化，执行对应的函数
+ */
+function changeQueue(){
     //添加监听事件
     form.addEventListener("click",function(e){
         if(e.target.localName == "button"){   //如果被点击的是button标签
@@ -88,9 +102,22 @@ function changeQueue(){
 }
 
 /**
+ * 队列中的某个数字被点击后删除
+ */
+function deleteAnyData(){
+    //添加监听事件
+    queueBox.addEventListener("click",function(e){
+        if(e.target.id ==""){   //如果被点击的是队列
+            dataStore.splice(e.target.className,1);   //删除被点击的数字
+            renderQueue();   //渲染列表
+        }
+    })
+}
+/**
 * 初始化函数
 */
 function init(){
     changeQueue();
+    deleteAnyData();
 }
 init();
